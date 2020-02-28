89eb2877e15dd6b8ee23e2d39c25ac3ec03be025
welunn <welun@live.cn>
2020-02-28 23:13:42 +0800

    django学习案例

---
+++ mysite/blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+# 定义UserInfo类，集成models.Model
+class UserInfo(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    gender = models.BooleanField(default=True)
+    age = models.IntegerField()
+    phone = models.CharField(max_length=20)
+
+
+# 定义博客类
+class BlogInfo(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=512)
+    createTime = models.DateTimeField()
+    createBy = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
+
