8ce46718d872025afc9d1e490604c0ffa2070214
welunn <welun@live.cn>
2020-02-29 18:14:13 +0800

    配置路由自动访问首页 使用render方法

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,14 +6,15 @@ from django.template import loader
 
 # 定义视图 index方法
 def index(request):
-    blogs = BlogInfo.objects.all()
-    print(blogs)
-    # 加载模板
-    temp = loader.get_template("blog/index.html")
-    # 使用模板 渲染动态数据
-    res = temp.render({"username": "管理员"})
-    # 把渲染结果返回也
-    return HttpResponse(res)
+    # blogs = BlogInfo.objects.all()
+    # print(blogs)
+    # # 加载模板
+    # temp = loader.get_template("blog/index.html")
+    # # 使用模板 渲染动态数据
+    # res = temp.render({"username": "管理员"})
+    # # 把渲染结果返回也
+    # return HttpResponse(res)
+    return render(request, "blog/index.html", {"username": "管理员"})
 
 
 # 获取用户列表
@@ -21,11 +22,13 @@ def list(request):
     # 获取所有的用户信息
     users = UserInfo.objects.all()
     # 加载模板
-    temp = loader.get_template("blog/list.html")
-    # 获取动态数据，渲染到静态页面
-    res = temp.render({"users": users})
-    # 把渲染的动态数据返回页面
-    return HttpResponse(res)
+    # temp = loader.get_template("blog/list.html")
+    # # 获取动态数据，渲染到静态页面
+    # res = temp.render({"users": users})
+    # # 把渲染的动态数据返回页面
+    # return HttpResponse(res)
+
+    return render(request, "blog/list.html", {"users": users})
 
 
 # 用户详细页面
@@ -33,11 +36,13 @@ def detail(request, id):
     print("这是传递的参数：%s"%(id,))
     userInfo = UserInfo.objects.get(pk=id)
     # 获取模板
-    temp = loader.get_template("blog/detail.html")
-    # 渲染数据
-    res = temp.render({"user": userInfo})
-    # 返回页面
-    return HttpResponse(res)
+    # temp = loader.get_template("blog/detail.html")
+    # # 渲染数据
+    # res = temp.render({"user": userInfo})
+    # # 返回页面
+    # return HttpResponse(res)
+
+    return render(request, "blog/detail.html", {"user": userInfo})
 
 
 # 根据id删除用户信息
