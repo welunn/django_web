93b09a5825e1d2fd2627cce97f7f8eadde02aac2
welunn <welun@live.cn>
2020-02-29 21:29:08 +0800

    重定向，用户添加，博客添加功能

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
+import datetime
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import UserInfo, BlogInfo
 from django.template import loader
 
@@ -27,8 +28,24 @@ def list(request):
     # res = temp.render({"users": users})
     # # 把渲染的动态数据返回页面
     # return HttpResponse(res)
-
     return render(request, "blog/list.html", {"users": users})
+
+
+# 添加用户
+def adduser(request):
+    # 如果是get请求跳转到用户注册页面
+    if request.method == "GET":
+        return render(request, "blog/adduser.html")
+    # 如果是post请求，把用户注册的信息提交到数据库，并且返回到列表页面
+    elif request.method == "POST":
+        userInfo = UserInfo()
+        userInfo.username = request.POST.get("username")
+        userInfo.password = request.POST.get("password")
+        userInfo.gender = request.POST.get("gender")
+        userInfo.age = request.POST.get("age")
+        userInfo.phone = request.POST.get("phone")
+        userInfo.save()
+        return HttpResponseRedirect("/list")
 
 
 # 用户详细页面
@@ -41,7 +58,6 @@ def detail(request, id):
     # res = temp.render({"user": userInfo})
     # # 返回页面
     # return HttpResponse(res)
-
     return render(request, "blog/detail.html", {"user": userInfo})
 
 
@@ -50,4 +66,31 @@ def delete(request, id):
     userInfo = UserInfo.objects.get(pk=id)
     userInfo.delete()
 
-    return list(request)
+    return HttpResponseRedirect("/list/")
+
+
+# 添加博客
+def addblog(request, id):
+    # 获取用户信息
+    userInfo = UserInfo.objects.get(pk=id)
+    if request.method == "GET":
+        # 跳转到博客添加页面
+        return render(request, "blog/addblog.html", {"user": userInfo})
+    elif request.method == "POST":
+        # 获取博客信息
+        blog = BlogInfo()
+        blog.title = request.POST.get("title")
+        blog.content = request.POST.get("content")
+        blog.createTime = datetime.datetime.now()
+        blog.createBy = userInfo
+        # 把博客信息保存到数据库
+        blog.save()
+        # 重定向到用户详细及博客列表显示页面
+        return HttpResponseRedirect("/detail/%s"%(id))
+
+
+# 删除博客信息
+def deleteblog(request, id):
+    blog = BlogInfo.objects.get(pk=id)
+    blog.delete()
+    return HttpResponseRedirect("/detail/%s"%(blog.createBy.id))
